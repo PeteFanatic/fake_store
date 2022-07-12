a0663a9565fa558bde4bfbccd2e05cff0dd10b7a
Pete Tiongzon <pete.tiongzon@gmail.com>
2022-07-13 02:23:56 +0800

    Tiongzon_FakeStore_Midterms

---
+++ android/app/src/main/kotlin/com/example/tiongzon_mobile3_midterms/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tiongzon_mobile3_midterms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/cartUpdate.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_null_aware_operators, file_names
+
+class CartUpdate {
+  int userId;
+  DateTime date;
+  List<dynamic> products;
+
+  CartUpdate({
+    required this.userId,
+    required this.date,
+    required this.products,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "userId": userId,
+      "date": date == null ? null : date.toIso8601String(),
+      "products": products,
+    };
+  }
+
+  factory CartUpdate.fromJson(Map<String, dynamic> json) {
+    return CartUpdate(
+      userId: json['userId'],
+      date: DateTime.parse(json['date']),
+      products: json['products'],
+    );
+  }
+}
